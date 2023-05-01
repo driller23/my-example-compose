54167d76b3dced4a85955dfb33fb05cf33f6d316
driller23 <igal.dahan@gmail.com>
2023-05-01 15:07:07 +0300

    sela course example for docker watch

---
+++ app/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const redis = require('redis');
+const app = express();
+const redisClient = redis.createClient({
+  host: 'redis',
+  port: 6379
+});
+
+
+app.get('/', function(req, res) {
+    redisClient.get('numVisits', function(err, numVisits) {
+        numVisitsToDisplay = parseInt(numVisits) + 1;
+        if (isNaN(numVisitsToDisplay)) {
+            numVisitsToDisplay = 1;
+        }
+        res.send('1 IS My App: Total number of visits is: ' + numVisitsToDisplay);
+        numVisits++;
+        redisClient.set('numVisits', numVisits);
+    });
+});
+
+app.listen(5000, function() {
+    console.log('App app is listening on port 5000');
+});
+
